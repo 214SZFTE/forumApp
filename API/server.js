be847b1a59b2e48f0ae2106de3d4cc8dcc6675d3
Tóth Károly Viktor <TothKarolyViktor@turr.hu>
2022-03-30 17:47:37 +0200

    FOR-15-node-js-server

--- API/server.js
+++ API/server.js
@@ -1,10 +1,22 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const mysql=require('mysql');
 var cors = require('cors');
 app.use(cors());
 app.use(express.json());
+const port = process.env.PORT || 3000;
 
-app.listen(8080, () => {
-    console.log(`Server listening on port 8080...`);
+
+var db = mysql.createPool({
+    connectionLimit: 10,
+    host: process.env.DBHOST,
+    user: process.env.DBUSER,
+    password: process.env.DBPASS,
+    database: process.env.DBNAME
+});
+
+
+app.listen(port, () => {
+    console.log(`Server listening on ${port}...`);
 })
