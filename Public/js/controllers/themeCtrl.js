e69b1f222acafc0266a9c865a339350ec2c22544
Viktor <TothKarolyViktor@turr.hu>
2022-04-19 21:40:57 +0200

    FOR-21-temak-kezelese-müködik

--- Public/js/controllers/themeCtrl.js
+++ Public/js/controllers/themeCtrl.js
@@ -15,14 +15,21 @@ app.controller('themeCtrl', function($scope,dbFactory,$rootScope) {
                     alert('Ez a téma már foglalt!');
                 } 
                 else {
-
                     let data = {
                             user_id:$rootScope.loggedUserID,	
                             name:$scope.themeTitle,
-                            timestamp:null
+                            timestamp:'NOW()'
                     }
 
                     dbFactory.insert('theme', data).then(function(res) {
+                        let newtheme={
+                                id:res.insertId,
+                                user_id:$rootScope.loggedUserID,
+                                name:data.name,
+                                timestamp:Date.now()
+                        }
+                        
+                        $scope.forums.push(newtheme)
                         $scope.themeTitle =null;
                         alert('Téma felvéve!');
                     });
@@ -36,22 +43,32 @@ app.controller('themeCtrl', function($scope,dbFactory,$rootScope) {
             $scope.themeTitle=res[0].name;
             $scope.ModThemeId=res[0].id;})
             $scope.upgrade=true;
+            $rootScope.forumThemeId=id;
     }
     
     $scope.modif=function(){
         let values={
             "name":$scope.themeTitle
         }
-        
         dbFactory.update('theme',$scope.ModThemeId,values).then(function(res){
-        console.log(res)})
+            for (let i = 0; i < $scope.forums.length; i++) {
+                if($scope.forums[i].id==$rootScope.forumThemeId){
+                    $scope.forums[i].name=values.name;
+                }
+                $rootScope.forumThemeId=null;
+            }
+        })
     }
 
 
     $scope.del=function(id){
         dbFactory.delete('theme',id).then(function(res){
+            for (let i = 0; i < $scope.forums.length; i++) {
+                if($scope.forums[i].id==id){
+                    $scope.forums.splice($scope.forums.indexOf($scope.forums[i]), 1);
+                }      
+            }
             alert("Téma törölve!");
         })
-
     }
 });
